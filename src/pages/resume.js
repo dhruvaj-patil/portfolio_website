cfda79cabfced8ea8f358e0c60fe2c2bec3df983
dhruvaj <dhruvaj.programmer@gmail.com>
2020-07-23 14:35:29 +0530

    resume page created

---
+++ src/pages/resume.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/layout/layout'
+
+export default function resume() {
+    return (
+        <Layout>
+            
+        </Layout>
+    )
+}
