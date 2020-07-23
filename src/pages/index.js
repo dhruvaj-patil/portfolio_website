84348069bce7b137d5409fe90c0850c9ec32f7e3
dhruvaj <dhruvaj.programmer@gmail.com>
2020-07-23 14:48:07 +0530

    Removed  ReactTyping Effecct to check Server Side Rendering

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Img from "gatsby-image";
 
 import SEO from "../components/seo";
 import style from "../components/pages/index/index.layout.module.scss";
-import ReactTypingEffect from "react-typing-effect";
+// import ReactTypingEffect from "react-typing-effect";
 
 const IndexPage = () => {
   const data = useStaticQuery(graphql`
@@ -42,41 +42,9 @@ const IndexPage = () => {
         <div className={` ${style.titleDiv} ${style.outlineContainer}`}>
           <h1 className={style.titleOutline}>Software</h1>
           <h1 className={style.titleOutline}>Developer</h1>
-          <div style={{ display: "flex", justifyContent: "space-between" }}>
-            {/* <h3 style={{ textAlign: "left", margin: "0px 0px 0px 20px" }}>
-              <ReactTypingEffect
-                style={{
-                  letterSpacing: "5px",
-                  color: "#F5BA57",
-                  textTransform: "uppercase",
-                  fontWeight: "bold",
-                  textAlign: "left",
-                }}
-                text={["Understand", "Provide"]} //text=["Hello.", "World!"]
-                eraseDelay={1500}
-                typingDelay={1000}
-                speed={176}
-              />
-            </h3>
-            {/* <br></br>
-          <br></br>
-           */}
-            <h3 style={{ textAlign: "right", margin: "0px 40px" }}>
-              <ReactTypingEffect
-                style={{
-                  letterSpacing: "5px",
-                  color: "#F5BA57",
-                  textTransform: "uppercase",
-                  fontWeight: "bold",
-                  textAlign: "left",
-                }}
-                text={["Vision", "Value."]} //text=["Hello.", "World!"]
-                speed={200}
-                typingDelay={1100}
-                eraseDelay={2000}
-              />
-            </h3> */}
-          </div>
+          {/* <div style={{ display: "flex", justifyContent: "space-between" }}>
+        
+          </div> */}
 
           {/*  ^^^^^^^ Change till here ^^^^^^^ */}
         </div>
